28fac0a66eed83e81fef65d0c4756394814fe553
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-08-02 00:37:50 +0900

    Practice 07: fetch API info and apply EffectHook

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,18 +1,29 @@
-import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Word from "./Word";
-import { useState } from "react-router-dom";
+
 export default function Day() {
   //dummy.words
   const { day } = useParams();
-  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  // const wordList = dummy.words.filter((word) => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/words?day=${day}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setWords(data);
+      });
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
